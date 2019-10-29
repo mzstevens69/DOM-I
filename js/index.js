8b838c3152491d4e8775e4560568f9f35664158d
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-10-28 19:17:09 -0500

    fixed Nav and added footer

--- js/index.js
+++ js/index.js
@@ -45,20 +45,36 @@ const siteContent = {
 const navLogo = document.querySelector('#logo-img');
 navLogo.src = 'img/logo.png';
 // /////a links///////////////
-const links = document.querySelectorAll('a');
-  for(let i = 0; i < links.length; i++) {
-    links[i] = ['nav'][`nav-item-];
+ const nav = document.querySelector('nav');
+  const firstA = document.createElement('a');
+  firstA.textContent = 'first link';
+  nav.prepend(firstA);
+  const LastA = document.createElement('a');
+  LastA.textContent = 'last link'
+  nav.append(LastA);
+
+  const allA = document.querySelectorAll('a');
+  allA[1].textContent = siteContent['nav']['nav-item-1'];
+  allA[2].textContent = siteContent['nav']['nav-item-2'];
+  allA[3].textContent = siteContent['nav']['nav-item-3'];
+  allA[4].textContent = siteContent['nav']['nav-item-4'];
+  allA[5].textContent = siteContent['nav']['nav-item-5'];
+  allA[6].textContent = siteContent['nav']['nav-item-6'];
+
+  allA.forEach(a => (a.style.color= 'green'));
+
+  
    
-  }
-});
+  
+
 
 // ////////header///////////////
 const hdrText = document.querySelector('.cta-text h1');
 hdrText.textContent = siteContent.cta['h1'];
 const ctaImg = document.querySelector('#cta-img');
 ctaImg.src = 'img/header-img.png';
-const theBtn = document.querySelector('buttton');
-theBtn.textContent = cta.button['Get Started'];
+const theBtn = document.querySelector('.cta-text button');
+theBtn.textContent = 'Get Started';
 
 
 
@@ -66,33 +82,52 @@ theBtn.textContent = cta.button['Get Started'];
 
 
 // /////////////main /////////////
-// const h4 = document.querySelectorAll('.main-content h4');
 
 
-// const paragraphs = document.querySelectorAll('main-content p');
+const h4 = document.querySelectorAll('.main-content h4');
+
+h4[0].textContent = siteContent['main-content']['features-h4'];
+h4[1].textContent = siteContent['main-content']['about-h4'];
+h4[2].textContent = siteContent['main-content']['services-h4'];
+h4[3].textContent = siteContent['main-content']['product-h4'];
+h4[4].textContent = siteContent['main-content']['vision-h4'];
+
+
+
+//================== Paragraphs
+const pTag = document.querySelectorAll('.main-content p');
+
+pTag [0].textContent = siteContent['main-content']['features-content'];
+pTag [1].textContent = siteContent['main-content']['about-content'];
+pTag [2].textContent = siteContent['main-content']['services-content'];
+pTag [3].textContent = siteContent['main-content']['product-content'];
+pTag [4].textContent = siteContent['main-content']['vision-content'];
+
+
+
 
 // ///////////mid pic//////////////////////////////
 const middleImage = document.querySelector('#middle-img');
 middleImage.src = 'img/mid-page-accent.jpg';
 
 // ////////////////////////////////
-// const topContent = document.querySelector(".top-content");
-// topContent.style.display = "flex";
+const topContent = document.querySelector(".top-content");
+topContent.style.display = "flex";
 // /////////////////////////////////////////
 // const bottomContent = document.querySelector(".bottom-content");
 // bottomContent.style.display = "flex";
 // /////////contact/////////////////
-// const contactSection = document.querySelectorAll(".contact");
-// const contactHeader = document.querySelector(".contact h4");
-// contactHeader.textContent = siteContent[`contact`]["contact-h4"];
+const contactSection = document.querySelectorAll(".contact");
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.textContent = siteContent[`contact`]["contact-h4"];
 
-// const contactPs = document.querySelectorAll(".contact p");
-// contactPs[0].textContent = siteContent[`contact`]["address"];
-// contactPs[1].textContent = siteContent[`contact`]["phone"];
-// contactPs[2].textContent = siteContent[`contact`]["email"];
+const contactPs = document.querySelectorAll(".contact p");
+contactPs[0].textContent = siteContent[`contact`]["address"];
+contactPs[1].textContent = siteContent[`contact`]["phone"];
+contactPs[2].textContent = siteContent[`contact`]["email"];
 
-// /////////Footer//////////////////////
-// const footer = document.querySelector("footer p");
-// footer.textContent = siteContent[`footer`]["copyright"];
+/////////Footer//////////////////////
+const footer = document.querySelector("footer p");
+footer.textContent = siteContent[`footer`]["copyright"];
 
 
